cf87e63804b682134bfc3737402a2fd12bd9f516
Abdullahazeem2 <abdullahazeemkhi@gmail.com>
2026-03-10 14:08:14 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//syncronous
+
+// console.log("one");
+// console.log("two");
+// console.log("three");
+
+//asyncronous
+
+console.log("one");
+console.log("two");
+setTimeout(() => { 
+    console.log("hello");
+}, 4000);
+console.log("three");
+console.log("four");
