6a1442578268a7aa1fa121e5dde69d913c3cfb97
Abdullahazeem2 <abdullahazeemkhi@gmail.com>
2026-03-17 19:18:14 +0500

    blank

--- app.js
+++ app.js
@@ -75,46 +75,32 @@
 // asyncfunc2().then((res) => {});
 // });
 
-
-
-
-
-
-
-
-
-
-// callback hell
-
 function getData(dataId,) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("data", dataId);
       resolve("sucess");
-    }, 5000);
+    }, 3000);
   });
-}
+};
 
-console.log("fetching data1.....");
-getData(1)
-
-.then((res) => {
-  console.log("fetching data2....."); 
-    return getData(2);
-    
-})
-.then((res) => {
-  console.log("fetching data3.....");
-    return getData(3);
-      
-}).then((res) => {
-  console.log(res);
-});
-
+// promise chaining
+// console.log("fetching data1.....");
+// getData(1)
+//  .then((res) => {
+//    console.log("fetching data2.....");
+//     return getData(2);
+//  })
+//  .then((res) => {
+//     console.log("fetching data3.....");
+//     return getData(3);
+//  })
+//  .then((res) => {
+//     console.log(res);
+//  });
 
 
-
-
+// callback hell
 // getData(1, () => {
 //     console.log("getting data2......");
 //   getData(2, () => {
